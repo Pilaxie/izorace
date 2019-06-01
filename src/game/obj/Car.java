100991f6e892d2751d23eaf3ba9595bff8e9c3a3
dyuzz16 <ciclopej@gmail.com>
2019-06-02 00:06:09 +0300

    Vars. Getters. Setters

--- src/game/obj/Car.java
+++ src/game/obj/Car.java
@@ -4,14 +4,29 @@ import game.phys.Hitbox;
 import nightingale.game.NGameObject;
 
 public class Car extends NGameObject{
-
 	protected Cars kind = null;
 	protected Hitbox hitbox = null;
 	
 	protected float maxV = 0;
+	protected float v = 0;
+	protected float turningSpeed = 0;
+	private float acceleration = 0;
+	private float brake = 0;
 	
 	// Getters
 	public Hitbox getHitbox() { return hitbox; }	
+	
+	public float         getMaxV() { return         maxV; }
+	public float               V() { return            v; }
+	public float getTurningSpeed() { return turningSpeed; }
+	public float getAcceleration() { return acceleration; }	
+	public float getBrake()        { return        brake; }
+	
+	// Setters
+	public void setV(float v) {
+		this.v = v;
+		if(V() > getMaxV()) this.v = getMaxV();
+	}
 	
 	public Car(Cars kind, int x, int y) {
 		super(x, y, kind.getSpecs().getWidth(), kind.getSpecs().getHeight());
@@ -22,6 +37,5 @@ public class Car extends NGameObject{
 	
 	public void update() {
 		hitbox.update();
-	}
-	
+	}	
 }
--- src/game/obj/Specifications.java
+++ src/game/obj/Specifications.java
@@ -3,14 +3,23 @@ package game.obj;
 final class Specifications{
 	private int width, height;
 	private float maxVelocity;
+	private float turningSpeed;
+	private float acceleration;
+	private float brake;
 	
-	public Specifications(int width, int height, float maxV) {
+	public Specifications(int width, int height, float maxV, float turningSpeed, float acceleration, float brake) {
 		this.width = width;
 		this.height = height;
 		this.maxVelocity = maxV;
+		this.turningSpeed = turningSpeed;
+		this.acceleration = acceleration;
+		this.brake = brake;
 	}
 	
-	public int getWidth()  { return       width; }
-	public int getHeight() { return      height; }
-	public float getMaxV() { return maxVelocity; }
+	public int   getWidth()        { return        width; }
+	public int   getHeight()       { return       height; }
+	public float getMaxV()         { return  maxVelocity; }
+	public float getTurningSpeed() { return turningSpeed; }
+	public float getAcceleration() { return acceleration; }	
+	public float getBrake()        { return        brake; }
 }
