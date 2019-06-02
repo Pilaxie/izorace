87c03612c59c96af9a5a25addf8d8177370195b4
dyuzz16 <ciclopej@gmail.com>
2019-06-02 13:10:24 +0300

    specs

--- src/game/obj/Cars.java
+++ src/game/obj/Cars.java
@@ -1,8 +1,8 @@
 package game.obj;
 
 public enum Cars {	
-	Long  (new Specifications(36, 60, 8, 1.9f, 1.7f, 3.3f)),
-	Hatch (new Specifications(30, 45, 6, 2.7f, 1.1f, 2.8f));
+	Long  (new Specifications(12, 20, 8, 1.9f, 1.7f, 3.3f)),
+	Hatch (new Specifications(10, 15, 6, 2.7f, 1.1f, 2.8f));
 	
 	private Specifications spec;
 	
