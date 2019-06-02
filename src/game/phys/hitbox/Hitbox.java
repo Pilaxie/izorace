f7fa6cc2b70559b7c34bd70e23d0afdbb889420b
dyuzz16 <ciclopej@gmail.com>
2019-06-02 14:44:21 +0300

    Add rect hitbox. Hitbox now is abstract

--- src/game/phys/hitbox/Hitbox.java
+++ src/game/phys/hitbox/Hitbox.java
@@ -4,12 +4,12 @@ import java.awt.Graphics;
 
 import nightingale.game.NGameObject;
 
-public class Hitbox extends NGameObject {
+public abstract class Hitbox extends NGameObject {
 
 	protected float angle = 0;
 	
-	public Point[] points = new Point[4];
-	public Line[] lines = new Line[4];
+	public Point[] points;
+	public Line[] lines;
 	
 	public float getAngle() { return angle; }
 	
@@ -25,45 +25,9 @@ public class Hitbox extends NGameObject {
 	
 	public float R() { return R; }
 	
-	public Hitbox(float x, float y, float w, float h) {
-		super(x, y, w, h);
-		
-		for(int i=0;i<points.length;i++)
-			points[i] = new Point(0, 0);
-		
-		R = (float)Math.sqrt( Math.pow(width/2 , 2) + 
-			      Math.pow(height/2, 2) );
-		update();
-	}
-	
-	public void update() {
-		//A
-		points[0].x = getCenterX() -  width/2;
-		points[0].y = getCenterY() - height/2;				        
-		//B
-		points[1].x = getCenterX() +  width/2;
-		points[1].y = getCenterY() - height/2;
-		//C
-		points[2].x = getCenterX() +  width/2;
-		points[2].y = getCenterY() + height/2;
-		//D
-		points[3].x = getCenterX() -  width/2;
-		points[3].y = getCenterY() + height/2;
-		//angle
-		for(int i=0;i<points.length;i++)
-		{
-			points[i].angle = (float)(Math.atan2(getCenterY()-points[i].y, getCenterX()-points[i].x));
-			points[i].x = getCenterX() - (float)(R*Math.cos(
-			(Math.PI*angle/180)+points[i].angle));
-			points[i].y = getCenterY() - (float)(R*Math.sin(
-			(Math.PI*angle/180)+points[i].angle));
-		}
+	public Hitbox() {}
 
-		lines[0] = new Line(points[0], points[1]);
-		lines[1] = new Line(points[1], points[2]);
-		lines[2] = new Line(points[2], points[3]);
-		lines[3] = new Line(points[3], points[0]);
-	}
+	public abstract void update();
 	
 	public boolean collideWith(Hitbox hitbox) {
 		for(int i=0;i<lines.length;i++)
---
+++ src/game/phys/hitbox/RectHitbox.java
@@ -0,0 +1,53 @@
+package game.phys.hitbox;
+
+public class RectHitbox extends Hitbox{
+
+	public RectHitbox(float x, float y, float w, float h) {
+		super();
+		setX(x);
+		setY(y);
+		setWidth(w);
+		setHeight(h);
+		
+		points = new Point[4];
+		lines = new Line[4];
+		
+		for(int i=0;i<points.length;i++)
+			points[i] = new Point(0, 0);
+		
+		R = (float)Math.sqrt( Math.pow(width/2 , 2) + 
+			      Math.pow(height/2, 2) );
+		update();
+	}
+
+	@Override
+	public void update()  {
+		//A
+		points[0].x = getCenterX() -  width/2;
+		points[0].y = getCenterY() - height/2;				        
+		//B
+		points[1].x = getCenterX() +  width/2;
+		points[1].y = getCenterY() - height/2;
+		//C
+		points[2].x = getCenterX() +  width/2;
+		points[2].y = getCenterY() + height/2;
+		//D
+		points[3].x = getCenterX() -  width/2;
+		points[3].y = getCenterY() + height/2;
+		//angle
+		for(int i=0;i<points.length;i++)
+		{
+			points[i].angle = (float)(Math.atan2(getCenterY()-points[i].y, getCenterX()-points[i].x));
+			points[i].x = getCenterX() - (float)(R*Math.cos(
+			(Math.PI*angle/180)+points[i].angle));
+			points[i].y = getCenterY() - (float)(R*Math.sin(
+			(Math.PI*angle/180)+points[i].angle));
+		}
+
+		lines[0] = new Line(points[0], points[1]);
+		lines[1] = new Line(points[1], points[2]);
+		lines[2] = new Line(points[2], points[3]);
+		lines[3] = new Line(points[3], points[0]);
+	}
+	
+}
